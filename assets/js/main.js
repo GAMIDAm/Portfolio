2a11477ef63e44c7c9572849e4b70bfe08cfc76e
GAMIDAm <miguel.bap.job@gmail.com>
2025-11-07 16:33:19 -0300

    Alterando as soft Skill com json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 
+function updateProfileInfo(profileData) {
+    const name = document.getElementById('profile.name');
+    const job = document.getElementById('profile.job');
+    const location = document.getElementById('profile.location');
+    const phone = document.getElementById('profile.phone');
+    const email = document.getElementById('profile.email');
+
+    name.innerText = profileData.nome;
+    job.innerText = profileData.titulo;
+    location.innerText = `${profileData.localidade.cidade}, ${profileData.localidade.estado}, ${profileData.localidade.pais}`;
+    phone.innerText = profileData.telefone;
+    phone.href = `tel:${profileData.telefone.replace(/\D/g, '')}`;
+    email.innerText = profileData.email;
+} 
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills');
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+}
+
 
 (async () =>  {
     
 
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
+
+    updateSoftSkills(profileData);
+
+    
 })()
